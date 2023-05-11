616ee6c27a0f5615506357c115da87d8f5558b97
martinvonhradsky <xhradsky@stuba.sk>
2023-05-11 18:55:08 +0200

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Create new file
+touch newfile.txt
+
+# Read contents of sample.txt and append "this text was copied"
+cat sample.txt >> newfile.txt
+echo "this text was copied" >> newfile.txt
